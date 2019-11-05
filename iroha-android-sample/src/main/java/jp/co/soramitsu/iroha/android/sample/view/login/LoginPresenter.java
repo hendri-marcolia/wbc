75d2e069dfcd4e4febc9cf92dd97d87f63363ceb
Ida Bagus Putu Peradnya Dinata <peradnya@gmail.com>
2019-11-05 23:12:52 +0700

    Add account checker when login by password

--- iroha-android-sample/src/main/java/jp/co/soramitsu/iroha/android/sample/view/login/LoginPresenter.java
+++ iroha-android-sample/src/main/java/jp/co/soramitsu/iroha/android/sample/view/login/LoginPresenter.java
@@ -2,22 +2,43 @@ package jp.co.soramitsu.iroha.android.sample.view.login;
 
 import javax.inject.Inject;
 
+import jp.co.soramitsu.iroha.android.sample.PreferencesUtil;
+import jp.co.soramitsu.iroha.android.sample.data.Validate;
+import jp.co.soramitsu.iroha.android.sample.interactor.ValidateAccountInteractor;
 import lombok.Setter;
 
 public class LoginPresenter {
+
+    private final PreferencesUtil preferencesUtil;
+    private final ValidateAccountInteractor validateAccountInteractor;
 
     @Setter
     private LoginView view;
 
     @Inject
-    public LoginPresenter() {}
+    public LoginPresenter(PreferencesUtil preferencesUtil,
+                          ValidateAccountInteractor validateAccountInteractor) {
+        this.preferencesUtil = preferencesUtil;
+        this.validateAccountInteractor = validateAccountInteractor;
+    }
 
     public void loginFromBiometric() {
         view.openMainView();
     }
 
     public void loginFromPassword(String accountId, String privateKey) {
-        view.openMainView();
+        Validate validate = new Validate();
+        validate.setAccountId(accountId);
+
+        validateAccountInteractor.execute(validate, validateResult -> {
+            if (validateResult.getHttpResult().equals("200")) {
+                view.openMainView();
+            } else {
+                // user not found;
+            }
+        }, throwable -> {
+            // Connection error
+        });
     }
 
 }
